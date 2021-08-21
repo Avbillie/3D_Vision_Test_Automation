1a16bc01a4ed331a92f11e213971a40486b2a839
Anthony_Billie_Jr <67110957+Avbillie@users.noreply.github.com>
2021-08-21 02:27:00 -0400

    updated selectors and test

--- src/tests/routes/customPcBuilder/customPcBuilder.test.ts
+++ src/tests/routes/customPcBuilder/customPcBuilder.test.ts
@@ -11,7 +11,8 @@ const driver: WebDriver = new Builder()
   .withCapabilities(Capabilities.chrome())
   .build();
 const cp = new CustomPcBuilderPage(driver);
-
+var start = new Date().getTime();
+    
 describe("Custom PC builder  page functionality", () => {
   beforeAll(async () => {
     await cp.navigateToHomePage();
@@ -20,22 +21,52 @@ describe("Custom PC builder  page functionality", () => {
     await driver.sleep(1000);
     await cp.click(cp.buildYourPcBtn);
     await driver.sleep(3000);
+  });
+  
+  test('Selected CPU component and set search filter options', async () => {
     await cp.click(cp.startPcBuilderBtn);
-  });
-
-  test(`Selected cpu filter options`, async () => {
     await cp.insertText(cp.searchWithinInputfield, 'amd ryzen threadripper\n')
-    await driver.sleep(2000)
     await cp.sortByBestRating()
-    await driver.sleep(2000);
   });
 
-  test(`Selected cpu`, async () => {
-    await cp.selectComponent()
-    await driver.sleep(2000)
+  test('Selected a CPU', async () => {
+    await cp.selectComponent(cp.cpuItem)
+  });
+  
+  test('Selected AMD MOTHERBOARD component and set search filter options', async () =>{
+    await cp.click(cp.motherboard)
+    await cp.sortByBestRating()
+  })
+
+  test('Selected a MOTHERBOARD', async () => {
+    await cp.selectComponent(cp.motherboardItem)
+    // await cp.click(cp.sideMenuArrow)
   });
 
-  afterAll(async () => {
-    await cp.quit();
+  test('Selected MEMORY component and set search filter options', async () =>{
+    await cp.click(cp.memory)
+    await cp.insertText(cp.searchWithinInputfield, 'g skill 64GB\n')
+    await cp.sortByBestRating()
+  })
+
+  test('Selected a MEMORY', async () => {
+    await cp.selectComponent(cp.memoryItem)
+    await cp.click(cp.sideMenuArrow)
   });
+
+  test('Selected GPU component and set search filter options', async () =>{
+    await cp.click(cp.videocard)
+    await cp.insertText(cp.searchWithinInputfield, 'nvidia 2080\n')
+    await cp.sortByBestRating()
+  })
+
+  test('Selected a GPU', async () => {
+    await cp.selectComponent(cp.videocardItem)
+  });
+
+  
+
+  // afterAll(async () => {
+  //   await cp.quit();
+  // });
 });
--- src/tests/routes/customPcBuilder/customPcBuilder.ts
+++ src/tests/routes/customPcBuilder/customPcBuilder.ts
@@ -13,36 +13,43 @@ export class CustomPcBuilderPage extends SignInPage {
   cpu: By = By.xpath("//div[contains(@class, 'pc-builder-icon')]//i[contains(@class, 'diy-cpu')]")
   motherboard: By = By.xpath("//div[contains(@class, 'pc-builder-icon')]//i[contains(@class, 'diy-motherboard')]");
   memory: By = By.xpath("//div[contains(@class, 'pc-builder-icon')]//i[contains(@class, 'diy-memory')]");
-  videocard: By = By.xpath("//div[contains(@class, 'pc-builder-icon')]//i[contains(@class, 'diy-gpu')]");
-  case: By = By.xpath("//div[contains(@class, 'pc-builder-icon')]//i[contains(@class, 'diy-case')]");
-  powersupply: By = By.xpath("//div[contains(@class, 'pc-builder-icon')]//i[contains(@class, 'diy-psu')]");
-  storage: By = By.xpath("//div[contains(@class, 'pc-builder-icon')]//i[contains(@class, 'diy-hdd')]");
-  cooling: By = By.xpath("//div[contains(@class, 'pc-builder-icon')]//i[contains(@class, 'diy-cooling')]");
-  os: By = By.xpath("//div[contains(@class, 'pc-builder-icon')]//i[contains(@class, 'diy-os')]");
-  accesories: By = By.xpath("//div[contains(@class, 'pc-builder-icon')]//i[contains(@class, 'diy-accesories')]");
-  monitorBtn: By = By.xpath("//div[contains(@class, 'pc-builder-item')]//a[contains(@href, 'https://www.newegg.com/tools/custom-pc-builder/pl/ID-3743?diywishlist=0')]")
+  videocard: By = By.xpath("//div[contains(@class, 'swiper-slide swiper-slide-next')]//i[contains(@class, 'diy-gpu')]");
+  case: By = By.xpath("//div[contains(@class, 'swiper-slide swiper-slide-next')]//i[contains(@class, 'diy-case')]");
+  powersupply: By = By.xpath("//div[contains(@class, 'swiper-slide swiper-slide-next')]//i[contains(@class, 'diy-psu')]");
+  storage: By = By.xpath("//div[contains(@class, 'swiper-slide swiper-slide-next')]//i[contains(@class, 'diy-hdd')]");
+  cooling: By = By.xpath("//div[contains(@class, 'swiper-slide swiper-slide-next')]//i[contains(@class, 'diy-cooling')]");
+  os: By = By.xpath("//div[contains(@class, 'swiper-slide swiper-slide-next')]//i[contains(@class, 'diy-os')]");  
+  // Selected items from component search results
+  cpuItem: By = By.xpath("//*[text() = 'AMD Ryzen Threadripper PRO 3955WX Desktop Processor']")
+  motherboardItem: By = By.xpath("//*[text() = 'ASUS Pro WS WRX80E-SAGE SE WIFI Extended ATX AMD Motherboard']")
+  memoryItem: By = By.xpath("//*[text() = 'G.SKILL Trident Z Royal Series Model F4-4000C18D-64GTRS']")
+  videocardItem: By = By.xpath("//*[text() = 'MSI RTX 2080 VENTUS 8G OC GeForce RTX 2080 Video Card']")
+
+  
+  // Component select button
+  selectBtn: By = By.xpath("//div[contains(@class, 'item-actions')]//i[contains(@class, 'fas fa-plus')]")
   // Component list 'sort by' dropdown selection menu
   sortBy: By = By.xpath("//div[contains(@class, 'list-tool-sortby')]")
   // Component list 'sort by' dropdown selection menu best rating option
   bestRatings: By = By.xpath("//option[contains(@value, '4')]")
   // Component list 'search within' input field
   searchWithinInputfield: By = By.xpath("//input[contains(@placeholder, 'Search Within')]")
-  // Component select button
-  selectBtn: By = By.xpath("//div[contains(@class, 'item-actions')]//i[contains(@class, 'fas fa-plus')]")
-  // First item in component list
-  firstItem: By = By.xpath(".//*[text() = 'AMD Ryzen Threadripper PRO 3955WX Desktop Processor']")
-  setUparray: any [] = [this.cpu, this.motherboard, this.videocard, this.case, this.powersupply, this.storage, this.cooling, this.os, this.accesories]
+  // Side menu control
+  sideMenuArrow: By = By.xpath("//a[contains(@class, 'pc-builder-control')]//i[contains(@class, 'fa fa-arrow-circle-right')]")
+  
+  
+  
+  setUparray: any [] = [this.cpu, this.motherboard, this.videocard, this.case, this.powersupply, this.storage, this.cooling, this.os]
   componentArray: any[] = []
-
   constructor(driver: WebDriver) {
     super(driver);
   }
   
-  async selectComponent(){
-    await this.driver.wait(until.elementIsVisible(await this.driver.findElement(this.firstItem)))
-    await this.click(this.firstItem)
+  async selectComponent(component:By){
+    await this.driver.wait(until.elementIsVisible(await this.driver.findElement(component)))
+    await this.click(component)
     await this.driver.sleep(2000)
-    await this.driver.wait(until.elementIsVisible(await this.driver.findElement(this.selectBtn)))
+    await this.driver.wait(until.elementIsVisible(await this.driver.findElement(component)))
     await this.click(this.selectBtn)
   }
   
